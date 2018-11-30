9a83f14495781a3052a1c14f15ea39a3020a944e
EwyBoy <ewyboy@gmail.com>
2018-11-30 22:44:49 +0100

    so far so good

--- www/js/elements.js
+++ www/js/elements.js
@@ -2,3 +2,9 @@ $(function(){
     $("#header").load("elements/header.html");
     $("#footer").load("elements/footer.html");
 });
+
+$(function () {
+    $.get("elements/meta.html", function (data) {
+        $("head").append(data);
+    });
+});
